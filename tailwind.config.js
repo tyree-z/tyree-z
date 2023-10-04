6d8ed911f15352b0327a3dae7b22308f5e2017b1
Tyree Zacharopoulos <tyree@tyree.ca>
2023-10-04 08:27:30 +0000

    Tailwind Fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {}
