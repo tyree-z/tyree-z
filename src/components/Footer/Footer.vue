7e55e814a5de9f6cb258a5c7cfa48589dfb27262
Tyree Zacharopoulos <tyree@tyree.ca>
2023-11-28 21:04:14 +0000

    fix(Footer)[TZRF-11]: Footer Changes
    Transparency Blur
    Version Display

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,16 +1,15 @@
 <template>
   <footer
-    class="fixed bottom-0 left-0 z-20 w-full p-4 bg-dark border-t border-gray-900 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600"
+    class="fixed bottom-0 left-0 z-20 w-full p-4 bg-opacity-90 backdrop-blur shadow md:flex md:items-center md:justify-between md:p-4"
   >
-    <span class="text-sm text-gray-700 sm:text-center dark:text-gray-400"
-      >© 2023
-      <router-link to="/" class="hover:underline text-primary">Tyree Zacharopoulos</router-link>.
-      All Rights Reserved.</span
-    >
+    <span class="text-xs text-base-content sm:text-center"
+      >© 2023 <router-link to="/" class="text-base-content">Tyree Zacharopoulos</router-link>. All
+      Rights Reserved.
+    </span>
     <ul
-      class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-700 dark:text-gray-400 sm:mt-0"
+      class="flex flex-wrap items-center mt-3 text-xs font-medium text-gray-700 dark:text-gray-400 sm:mt-0"
     >
-      <li>
+      <!-- <li>
         <a href="#" class="mr-4 hover:underline md:mr-6">About</a>
       </li>
       <li>
@@ -21,6 +20,9 @@
       </li>
       <li>
         <a href="#" class="hover:underline">Contact</a>
+      </li> -->
+      <li class="ml-6">
+        <p id="packageVersion"></p>
       </li>
     </ul>
   </footer>
@@ -31,9 +33,9 @@ import packageJson from '../../../package.json'
 export default {
   name: 'FooterComponent',
   created() {
-    // this.$nextTick(() => {
-    //   document.getElementById('packageVersion').innerHTML = 'v' + packageJson.version
-    // })
+    this.$nextTick(() => {
+      document.getElementById('packageVersion').innerHTML = 'v' + packageJson.version
+    })
   }
 }
 </script>
